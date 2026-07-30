ae0f5a21c56f6e1f30dadab29f09f66e5041e70a
Callum Wellwood-Kane <callum.kane@drive.com.au>
2026-07-30 10:27:44 +1000

    show herdr instead of pwd

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -185,7 +185,12 @@ if status is-interactive
 
     if set -q ZELLIJ
       function zellij_rename_tab_pwd --on-event fish_prompt
-          zellij action rename-tab (basename (pwd))
+          set -l tty (tty | string replace /dev/ '')
+          if pgrep -t $tty -x herdr >/dev/null
+              zellij action rename-tab herdr
+          else
+              zellij action rename-tab (basename (pwd))
+          end
       end
     end
 
