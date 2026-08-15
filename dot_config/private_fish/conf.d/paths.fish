65ee75e3a4b951f2f6ef8e5613fe800f54ddd4ef
Callum Wellwood-Kane <business@callumwk.com>
2026-08-15 15:50:11 +1000

    updated path mapping

--- dot_config/private_fish/conf.d/paths.fish
+++ dot_config/private_fish/conf.d/paths.fish
@@ -3,38 +3,23 @@ switch (uname)
 
       fish_add_path /opt/homebrew/bin
 
-      source "$HOME/.cargo/env.fish"
-
 
       if test -d $HOME/.dotnet
         set -x DOTNET_ROOT /opt/homebrew/opt/dotnet/libexec
         fish_add_path $DOTNET_ROOT
       end
 
-      set MYSQL_HOME "/opt/homebrew/opt/mysql-client/bin"
-      if test -d $MYSQL_HOME
-        fish_add_path $MYSQL_HOME
-      end
-
-      set JAVA_HOME "/opt/homebrew/opt/openjdk@21/bin"
-      if test -d $JAVA_HOME
-        fish_add_path $JAVA_HOME
-      end
-
-      set ORBSTACK_BIN "$HOME/.orbstack/bin"
-      if test -d $ORBSTACK_BIN
-        set -x ORBSTACK_BIN $ORBSTACK_BIN
+      if test -d $HOME/.orbstack/bin
+        set -x ORBSTACK_BIN $HOME/.orbstack/bin
         fish_add_path $ORBSTACK_BIN
       end
 
-      set RUSTUP_ROOT "/opt/homebrew/opt/rustup/bin"
-      if test -d $RUSTUP_ROOT
-        fish_add_path $RUSTUP_ROOT
-      end
-
-
-      if test -d $HOME/.rbenv/shims
-        fish_add_path $HOME/.rbenv/shims
+      for dir in \
+          /opt/homebrew/opt/mysql-client/bin \
+          /opt/homebrew/opt/openjdk@21/bin \
+          /opt/homebrew/opt/rustup/bin \
+          $HOME/.rbenv/shims
+        test -d $dir; and fish_add_path $dir
       end
 
 
@@ -67,39 +52,17 @@ if test -d $HOME/.config/android-sdk
   fish_add_path $ANDROID_HOME
 end
 
-if test -f $HOME/.local/bin/claude
-    fish_add_path $HOME/.local/bin
+for dir in \
+    $HOME/.local/bin \
+    $HOME/.spin/bin \
+    $HOME/.composer/vendor/bin \
+    $HOME/.volta/bin \
+    $HOME/.dotnet/tools \
+    $HOME/fvm/default/bin \
+    $HOME/.pub-cache/bin
+  test -d $dir; and fish_add_path $dir
 end
 
-if test -d $HOME/.spin/bin
-  fish_add_path $HOME/.spin/bin
-end
-
-if test -d $HOME/.composer/vendor/bin
-  fish_add_path $HOME/.composer/vendor/bin
-end
-
-if test -d $HOME/.volta/bin
-  fish_add_path $HOME/.volta/bin
-end
-
-
-## .NET Tools ##
-if test -d $HOME/.dotnet/tools
-  fish_add_path $HOME/.dotnet/tools
-end
-
-## Flutter ##
-if test -d $HOME/fvm/default/bin
-  fish_add_path $HOME/fvm/default/bin
-end
-
-set DART_PUB_PACKAGES "$HOME/.pub-cache/bin"
-if test -d $DART_PUB_PACKAGES
-  fish_add_path $DART_PUB_PACKAGES
-end
-
-## PyEnv ##
 if test -d $HOME/.pyenv
   set -x PYENV_ROOT $HOME/.pyenv
   fish_add_path $PYENV_ROOT/bin
